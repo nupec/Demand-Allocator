bfcf338fc480139707af8a583faec5845d47d859
Pablo Oliveira <125398395+DevPabloOliveira@users.noreply.github.com>
2024-09-06 13:17:40 -0400

    Add new KNN demand allocation route in main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import geopandas as gpd
 from unidecode import unidecode
 
 from app.config import settings
-from app.allocation.allocation import allocate_demands
+from app.allocation.allocation import allocate_demands,  allocate_demands_knn
 from app.geoprocessing.geoprocessing import process_geometries
 from app.utils.utils import infer_column, calculate_geodesic_distance
 
@@ -24,11 +24,9 @@ def allocate_demands_api(
     establishments_gdf = gpd.read_file(establishments_file.file)
     demands_gdf = gpd.read_file(demands_file.file)
 
-    # Calculate centroids if necessary
     establishments_gdf = process_geometries(establishments_gdf)
     demands_gdf = process_geometries(demands_gdf)
 
-    # Infer column names
     col_demand_id = infer_column(demands_gdf, settings.DEMAND_ID_POSSIBLE_COLUMNS)
     col_name = infer_column(establishments_gdf, settings.NAME_POSSIBLE_COLUMNS)
     col_city = infer_column(establishments_gdf, settings.CITY_POSSIBLE_COLUMNS)
@@ -38,27 +36,60 @@ def allocate_demands_api(
     if not col_demand_id or not col_name or not col_city or not col_state_establishment or not col_state_demand:
         return {"error": "Could not infer all necessary columns. Please check the input data."}
 
-    # Filter establishments by state
     establishments_gdf = establishments_gdf[establishments_gdf[col_state_establishment] == state]
     print(f"Number of establishments after filtering by state '{state}': {len(establishments_gdf)}")
 
-    # Filter establishments by city, if applicable
     if city:
         city = unidecode(city.lower())
         establishments_gdf = establishments_gdf[establishments_gdf[col_city].apply(lambda x: unidecode(x.lower())) == city]
         print(f"Number of establishments after filtering by city '{city}': {len(establishments_gdf)}")
 
-    # Filter demands by state
     demands_gdf = demands_gdf[demands_gdf[col_state_demand] == state]
     print(f"Number of demands after filtering by state '{state}': {len(demands_gdf)}")
 
-    # Filter demands by city, if applicable
     if city:
         demands_gdf = demands_gdf[demands_gdf['NM_MUN'].apply(lambda x: unidecode(x.lower())) == city]
         print(f"Number of demands after filtering by city '{city}': {len(demands_gdf)}")
 
-    # Perform demand allocation
     result_df = allocate_demands(demands_gdf, establishments_gdf, col_demand_id, col_name, col_city)
 
-    # Return the result
+    return result_df.to_dict(orient='records')
+
+
+@app.post("/allocate_demands_knn/")
+def allocate_demands_knn_api(
+    establishments_file: UploadFile = File(...),
+    demands_file: UploadFile = File(...),
+    state: str = Query(...),
+    city: Optional[str] = None,
+    k: int = Query(1) 
+):
+    establishments_gdf = gpd.read_file(establishments_file.file)
+    demands_gdf = gpd.read_file(demands_file.file)
+
+    establishments_gdf = process_geometries(establishments_gdf)
+    demands_gdf = process_geometries(demands_gdf)
+
+    col_demand_id = infer_column(demands_gdf, settings.DEMAND_ID_POSSIBLE_COLUMNS)
+    col_name = infer_column(establishments_gdf, settings.NAME_POSSIBLE_COLUMNS)
+    col_city = infer_column(establishments_gdf, settings.CITY_POSSIBLE_COLUMNS)
+    col_state_establishment = infer_column(establishments_gdf, settings.STATE_POSSIBLE_COLUMNS)
+    col_state_demand = infer_column(demands_gdf, settings.STATE_POSSIBLE_COLUMNS)
+
+    if not col_demand_id or not col_name or not col_city or not col_state_establishment or not col_state_demand:
+        return {"error": "Could not infer all necessary columns. Please check the input data."}
+
+    establishments_gdf = establishments_gdf[establishments_gdf[col_state_establishment] == state]
+
+    if city:
+        city = unidecode(city.lower())
+        establishments_gdf = establishments_gdf[establishments_gdf[col_city].apply(lambda x: unidecode(x.lower())) == city]
+
+    demands_gdf = demands_gdf[demands_gdf[col_state_demand] == state]
+
+    if city:
+        demands_gdf = demands_gdf[demands_gdf['NM_MUN'].apply(lambda x: unidecode(x.lower())) == city]
+
+    result_df = allocate_demands_knn(demands_gdf, establishments_gdf, col_demand_id, col_name, col_city, k)
+
     return result_df.to_dict(orient='records')

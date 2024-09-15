2f24e0cad7f45c487335f772bee37b7f6d8fe1cd
Pablo Oliveira <125398395+DevPabloOliveira@users.noreply.github.com>
2024-09-15 08:51:25 -0400

    Update allocation.py

--- app/allocation/allocation.py
+++ app/allocation/allocation.py
@@ -27,31 +27,3 @@ def allocate_demands(demands_gdf, establishments_gdf, col_demand_id, col_name, c
         })
 
     return pd.DataFrame(allocation)
-
-def allocate_demands_knn(demands_gdf, establishments_gdf, col_demand_id, col_name, col_city, k=1):
-    allocation = []
-
-    demands_coords = np.array(list(zip(demands_gdf.geometry.x, demands_gdf.geometry.y)))
-    establishments_coords = np.array(list(zip(establishments_gdf.geometry.x, establishments_gdf.geometry.y)))
-    
-    knn = knnW_from_array(establishments_coords, k=k)
-    
-    for i, demand in enumerate(demands_gdf.itertuples(), 1):
-        demand_coord = np.array([demand.geometry.x, demand.geometry.y])
-        
-        neighbors_idx = knn.neighbors[i]
-        closest_establishments = establishments_gdf.iloc[neighbors_idx]
-        
-        closest_establishment = closest_establishments.iloc[0]  
-        
-        allocation.append({
-            'Sector_ID': getattr(demand, col_demand_id),
-            'Establishment': getattr(closest_establishment, col_name),
-            'Establishment_City': getattr(closest_establishment, col_city),
-            'Distance': calculate_geodesic_distance(
-                (demand.geometry.y, demand.geometry.x), 
-                (closest_establishment.geometry.y, closest_establishment.geometry.x)
-            )
-        })
-
-    return pd.DataFrame(allocation)

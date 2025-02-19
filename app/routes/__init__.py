021764065ea6712ac2411737c8a2a4a5707659cd
Pablo Oliveira <125398395+DevPabloOliveira@users.noreply.github.com>
2025-02-19 14:21:00 -0400

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
-from .allocation_route import router as allocation_router
 from .knn_route import router as knn_router
-from .distance_matrix_route import router as distance_matrix_router
+from .methods_route import router as methods_router
+
+from .localities_route import router as localities_router
 
 
 router = APIRouter()
-router.include_router(allocation_router, prefix="/allocation")
-router.include_router(knn_router, prefix="/knn")
-router.include_router(distance_matrix_router, prefix="/network_analysis")
+router.include_router(knn_router, prefix="/knn_model")
+router.include_router(methods_router, prefix="/methods")
+
+router.include_router(localities_router, prefix="/localities")
